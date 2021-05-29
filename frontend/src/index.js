a8cb42034331b25879856b9f2b72dbb188a5beee
yotaml <yotaml@jfrog.com>
2021-05-29 17:12:58 +0300

    added frontend components, edited gitignore.

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FadeIn from 'react-fade-in';
+import reportWebVitals from './reportWebVitals';
+// import VerticalLinearStepper from './components/vertical_liniar_Stepper'
+import MainWindow from './components/main_window'
+
+ReactDOM.render(
+  <React.StrictMode>
+      <FadeIn transitionDuration={1000}>
+        <MainWindow />
+      </FadeIn>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
